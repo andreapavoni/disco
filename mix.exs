8d32a960b8c0f035264bf1bf3dc2fbc180684c4b
Andrea Pavoni <andrea.pavoni@gmail.com>
2018-12-11 00:33:32 +0100

    version 0.1.2

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule Disco.MixProject do
       app: :disco,
       description: description(),
       package: package(),
-      version: "0.1.0",
+      version: "0.1.2",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
