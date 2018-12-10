efe305b14e81e6968296667fbc036a122cafa853
Andrea Pavoni <andrea.pavoni@gmail.com>
2018-12-11 00:30:44 +0100

    enable repo start only for test env

--- lib/disco/application.ex
+++ lib/disco/application.ex
@@ -6,11 +6,17 @@ defmodule Disco.Application do
   use Application
 
   def start(_type, _args) do
-    children = [
-      Disco.Repo
-    ]
+    children = child_apps(Mix.env())
 
     opts = [strategy: :one_for_one, name: Disco.Supervisor]
     Supervisor.start_link(children, opts)
   end
+
+  defp child_apps(:test) do
+    [
+      Disco.Repo
+    ]
+  end
+
+  defp child_apps(_), do: []
 end
