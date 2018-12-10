8557526a4500b5ce1eab5b92d084f8fe3866b924
Andrea Pavoni <andrea.pavoni@gmail.com>
2018-12-10 01:21:17 +0100

    add orchestrator documentation

--- lib/disco/orchestrator.ex
+++ lib/disco/orchestrator.ex
@@ -1,6 +1,33 @@
 defmodule Disco.Orchestrator do
   @moduledoc """
-  Orchestrates several aggregates together.
+  Orchestrates multiple `Disco.Aggregate` together.
+
+  This module is a `GenServer` implementation that can handle more than one aggregate in
+  the same place. A common problem, for example, arises when working with [umbrella projects](https://elixir-lang.org/getting-started/mix-otp/dependencies-and-umbrella-projects.html#umbrella-projects),
+  where a module can be used to run all the commands and queries without exposing internal
+  list of available aggregates.
+
+  ## Usage example
+
+  The usage is similar to `Disco.Aggregate`, except that this is a `GenServer`, so it needs
+  to be started before using it. In a real scenario, maybe you'll want to put in under
+  some supervisor.
+
+    _NOTE: `Disco.Factories.ExampleAggregate` has been defined in `test/support/examples/example_aggregate.ex`._
+
+  ```
+  iex> {:ok, pid} = Disco.Orchestrator.start_link [Disco.Factories.ExampleAggregate]
+  iex> is_pid(pid)
+  true
+  iex> Disco.Orchestrator.commands()
+  [:do_something]
+  iex> Disco.Orchestrator.routes()
+  [:find_something]
+  iex> Disco.Orchestrator.dispatch(:do_something, %{foo: "bar"})
+  {:ok, %Disco.Factories.ExampleAggregate{foo: "bar", id: "4fd98a9e-8d6f-4e35-a8fc-aca5544596cb"}}
+  iex> Disco.Orchestrator.query(:find_something, %{foo: "bar"})
+  %{foo: "bar"}
+  ```
   """
 
   use GenServer
@@ -31,7 +58,8 @@ defmodule Disco.Orchestrator do
   Executes a command if available. It runs sync by default, use `async: true` option to
   run async.
   """
-  @spec dispatch(atom(), map(), async: boolean()) :: :ok | {:ok, map()} | {:error, any()}
+  @spec dispatch(command :: atom(), params :: map(), async: boolean()) ::
+          :ok | {:ok, map()} | {:error, any()}
   def dispatch(command, params, opts \\ []) do
     with {:ok, {aggregate, module}} <- GenServer.call(__MODULE__, {:route_command, command}),
          {:ok, cmd} <- params |> module.new() |> module.validate() do
@@ -44,9 +72,9 @@ defmodule Disco.Orchestrator do
   end
 
   @doc """
-  Executes a query on the aggregate.
+  Executes a query on one of the available aggregates.
   """
-  @spec query(atom(), map()) :: any() | [any]
+  @spec query(query :: atom(), params :: map()) :: result :: any() | [any]
   def query(query, params) do
     with {:ok, {_, module}} <- GenServer.call(__MODULE__, {:route_query, query}),
          {:ok, query} <- params |> module.new() |> module.validate() do
